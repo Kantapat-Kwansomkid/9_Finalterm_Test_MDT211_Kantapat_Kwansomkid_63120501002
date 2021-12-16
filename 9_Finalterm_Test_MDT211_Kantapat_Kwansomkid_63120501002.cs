e9523d374ca8bdbb957ccdfe92b7c42241ebec18
Kantapat-Kwansomkid <72593812+Kantapat-Kwansomkid@users.noreply.github.com>
2021-12-16 12:39:01 +0700

    Add files via upload

---
+++ 9_Finalterm_Test_MDT211_Kantapat_Kwansomkid_63120501002.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace _9_Finalterm_Test_MDT211_Kantapat_Kwansomkid_63120501002
+{
+    private Classic Skill;
+
+    public void Push(Node node)
+    {
+        if (root == null)
+        {
+            root = node;
+        }
+        else
+        {
+            Node ptr = root;
+            while (ptr.Next != null)
+            {
+                ptr = ptr.Next;
+            }
+            ptr.Next = node;
+        }
+    }
+
+    public Node Pop()
+    {
+        Node node = root;
+        if (root != null)
+        {
+            root = root.Next;
+            node.Next = null;
+        }
+        return node;
+    }
+
+    public int GetLength()
+    {
+        if (root == null)
+        {
+            return 0;
+        }
+        else
+        {
+            Node ptr = root;
+            int length = 1;
+            while (ptr.Next != null)
+            {
+                ptr = ptr.Next;
+                length++;
+            }
+            return length;
+        }
+    }
+
+    public override string ToString()
+    {
+        string message = "";
+        if (root != null)
+        {
+            Node ptr = root;
+            message = message + "," + root;
+            while (ptr.Next != null)
+            {
+                ptr = ptr.Next;
+                message = message + "," + ptr;
+            }
+        }
+        return message;
+    }
+}
